f330ddb11255f3d74ce5e6c65d9f32916bed060b
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-23 11:38:09 +0100

    Add comments to BaseAPIClient

--- PokeAPIConnector/PokeAPIConnector/Common/Base/BaseAPIClient.swift
+++ PokeAPIConnector/PokeAPIConnector/Common/Base/BaseAPIClient.swift
@@ -9,82 +9,69 @@ import UIKit
 import Alamofire
 import Combine
 
-
 class BaseAPIClient {
-
+    
+    // MARK: - Properties
+    
     private var isReachable: Bool = true
-    private var sesionManager: Alamofire.Session!
-
+    private var sessionManager: Alamofire.Session!
+    
     private var baseURL: URL {
-
+        // URL base de la API
         if let url = URL(string: "https://pokeapi.co/api/v2/") {
             return url
         } else {
             return URL(string: "")!
         }
     }
-
+    
+    // MARK: - Initialization
+    
     init() {
-
-        // Control para el SSL pinning
-//        let serverTrustPolicies: [String: ServerTrustEvaluating] = [
-//            "api.myjson.com": PinnedCertificatesTrustEvaluator(),
-//            ]
-//
-//        self.sesionManager = Session(
-//            serverTrustManager: ServerTrustManager(evaluators: serverTrustPolicies)
-//        )
-
-        self.sesionManager = Session()
+        // Inicialización del SessionManager de Alamofire y configuración del listener de conectividad
+        self.sessionManager = Session()
         startListenerReachability()
     }
-
-
-    // MARK: - Public method
-
+    
+    // MARK: - Public Methods
+    
+    // Método para manejar los errores de la API
     func handler(error: Error?) -> BaseError? {
-
-        if !self.isReachable { return BaseError.noInternetConnection }
-        var baseError: BaseError?
-
-        if error != nil {
-            baseError = BaseError.generic
+        // Comprueba si hay conexión a Internet
+        if !self.isReachable {
+            return BaseError.noInternetConnection
         }
-
-        return baseError
+        
+        // Comprueba si hay un error genérico
+        if error != nil {
+            return BaseError.generic
+        }
+        
+        return nil
     }
     
+    // Método para manejar la respuesta de la API
     func handleResponse<A: Codable>(success: @escaping (A) -> Void, failure: @escaping (BaseError) -> Void, dataResponse: AFDataResponse<A>) {
-        
-        if let baseError = self.handler(error: dataResponse.error)  {
+        // Manejar los errores de la API
+        if let baseError = self.handler(error: dataResponse.error) {
             failure(baseError)
-            
         } else if let responseObject: A = dataResponse.value {
             success(responseObject)
-            
         } else {
             failure(.generic)
         }
     }
-
-    func request(_ relativePath: String?,
-                 method: HTTPMethod = .get,headers: [String: String] = [:],
-                 parameters: Parameters? = nil, encoding: ParameterEncoding = JSONEncoding.default) -> DataRequest {
-
+    
+    // Método para realizar una solicitud a la API
+    func request(_ relativePath: String?, method: HTTPMethod = .get, headers: [String: String] = [:], parameters: Parameters? = nil, encoding: ParameterEncoding = JSONEncoding.default) -> DataRequest {
         let urlAbsolute = baseURL.appendingPathComponent(relativePath!)
-        return sesionManager.request(urlAbsolute, method: method, parameters: parameters, encoding: encoding, headers: HTTPHeaders(headers)).cURLDescription { p in
-             print(p)
+        return sessionManager.request(urlAbsolute, method: method, parameters: parameters, encoding: encoding, headers: HTTPHeaders(headers)).cURLDescription { p in
+            print(p)
         }
     }
     
-    func requestPublisher<T: Decodable>(relativePath: String?,
-                                        method: HTTPMethod = .get,
-                                        parameters: Parameters? = nil,
-                                        urlEncoding: ParameterEncoding = JSONEncoding.default,
-                                        type: T.Type = T.self,
-                                        base: URL? = URL(string: "https://pokeapi.co/api/v2/"),
-                                        customHeaders: HTTPHeaders? = nil) -> AnyPublisher<T, BaseError> {
-        
+    // Método para realizar una solicitud a la API usando Combine
+    func requestPublisher<T: Decodable>(relativePath: String?, method: HTTPMethod = .get, parameters: Parameters? = nil, urlEncoding: ParameterEncoding = JSONEncoding.default, type: T.Type = T.self, base: URL? = URL(string: "https://pokeapi.co/api/v2/"), customHeaders: HTTPHeaders? = nil) -> AnyPublisher<T, BaseError> {
         guard let url = base, let path = relativePath else {
             return Fail(error: BaseError.generic).eraseToAnyPublisher()
         }
@@ -92,43 +79,39 @@ class BaseAPIClient {
         guard let urlAbsolute = url.appendingPathComponent(path).absoluteString.removingPercentEncoding else {
             return Fail(error: BaseError.generic).eraseToAnyPublisher()
         }
-
-        return sesionManager.request(urlAbsolute, method: method, parameters: parameters, encoding: urlEncoding, headers: nil)
+        
+        return sessionManager.request(urlAbsolute, method: method, parameters: parameters, encoding: urlEncoding, headers: nil)
             .validate()
 #if DEBUG
             .cURLDescription(on: .main, calling: { p in print(p) })
 #endif
             .publishDecodable(type: T.self, emptyResponseCodes: [204])
-            .tryMap({ response in
-               // print(String(decoding: response.data!, as: UTF8.self))
+            .tryMap { response in
                 switch response.result {
                 case let .success(result):
                     return result
                 case let .failure(error):
-                    // print(String(decoding: response.data!, as: UTF8.self))
                     return error as! T
                 }
-            })
-            .mapError({ [weak self] error in
+            }
+            .mapError { [weak self] error in
                 guard let self = self else { return .generic }
                 return self.handler(error: error) ?? .generic
-            })
+            }
             .eraseToAnyPublisher()
     }
-
-    // MARK: - Private Method
-
+    
+    // MARK: - Private Methods
+    
+    // Método para iniciar el listener de conectividad
     private func startListenerReachability() {
-
-        let net = NetworkReachabilityManager()
-        net?.startListening(onUpdatePerforming: {  status in
-            if net?.isReachable ?? false {
+        let networkReachability = NetworkReachabilityManager()
+        networkReachability?.startListening(onUpdatePerforming: { status in
+            if networkReachability?.isReachable ?? false {
                 self.isReachable = true
-
             } else {
                 self.isReachable = false
             }
         })
     }
-
 }
