d41b9ce9de8548b85418d316b8075412f7e475b8
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-23 11:36:01 +0100

    Add comments to BaseError

--- PokeAPIConnector/PokeAPIConnector/Common/Base/BaseError.swift
+++ PokeAPIConnector/PokeAPIConnector/Common/Base/BaseError.swift
@@ -7,17 +7,20 @@
 
 import Foundation
 
+// Enumeración que representa los posibles errores base
 enum BaseError: Error {
-    case generic
-    case noInternetConnection
+    case generic  // Error genérico
+    case noInternetConnection  // Falta de conexión a Internet
     
+    // Método para obtener una descripción del error
     func description() -> String {
         
         var description: String = ""
         
+        // Switch para determinar la descripción del error
         switch self {
-        case .generic: description = "Error generico"
-        case .noInternetConnection: description = "NO hay conexion"
+        case .generic: description = "Error genérico"
+        case .noInternetConnection: description = "No hay conexión a Internet"
         }
         
         return description
