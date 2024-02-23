d5a8919508f11a1159c78a20a6ac67d9fd389f1b
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-23 09:17:08 +0100

    Add some background colour and navigation controller colors

--- PokeAPIConnector/PokeAPIConnector/MVCThirdForm/Controller/PokedexController.swift
+++ PokeAPIConnector/PokeAPIConnector/MVCThirdForm/Controller/PokedexController.swift
@@ -19,9 +19,23 @@ class PokedexController: UICollectionViewController {
         configureViewComponents()
     }
     
+    // MARK: - Selectors
+    
+    @objc func showSearchBar() {
+        print(123)
+    }
+    
     // MARK: - Helper Functions
     
     func configureViewComponents() {
         collectionView.backgroundColor = .white
+        
+        navigationController?.navigationBar.barTintColor = .mainPink()
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationItem.title = "Pokedex"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar))
+        navigationItem.rightBarButtonItem?.tintColor = .white
     }
 }
--- PokeAPIConnector/PokeAPIConnector/MVCThirdForm/Utils/Extensions.swift
+++ PokeAPIConnector/PokeAPIConnector/MVCThirdForm/Utils/Extensions.swift
@@ -12,4 +12,8 @@ extension UIColor {
     static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
     }
+    
+    static func mainPink() -> UIColor {
+        return UIColor.rgb(red: 221, green: 94, blue: 86)
+    }
 }
