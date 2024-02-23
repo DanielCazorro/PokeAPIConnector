53f7f60ad4558c14230575adbc5c9584f7db9968
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-23 11:40:54 +0100

    Add comments to Pokemon Structure

--- PokeAPIConnector/PokeAPIConnector/Models/Pokemon.swift
+++ PokeAPIConnector/PokeAPIConnector/Models/Pokemon.swift
@@ -7,17 +7,20 @@
 
 import Foundation
 
+// Estructura que representa un Pokemon
 struct Pokemon: Codable {
-    let name: String
-    let abilities: [Ability]
+    let name: String  // Nombre del Pokemon
+    let abilities: [Ability]  // Habilidades del Pokemon
 }
 
+// Estructura que representa una habilidad de un Pokemon
 struct Ability: Codable {
-    let ability: AbilityName
-    let is_hidden: Bool
-    let slot: Int
+    let ability: AbilityName  // Nombre de la habilidad
+    let is_hidden: Bool  // Indica si la habilidad está oculta
+    let slot: Int  // Ranura de la habilidad
 }
 
+// Estructura que representa el nombre de una habilidad
 struct AbilityName: Codable {
-    let name, url: String
+    let name, url: String  // Nombre y URL de la habilidad
 }
