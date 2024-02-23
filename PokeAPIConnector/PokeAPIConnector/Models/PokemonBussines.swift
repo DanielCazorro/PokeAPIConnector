649a1a2edd9ce7d0012ac2510f0fff6662f6360a
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-23 11:39:42 +0100

    Add comments to PokemonBusiness

--- PokeAPIConnector/PokeAPIConnector/Models/PokemonBussines.swift
+++ PokeAPIConnector/PokeAPIConnector/Models/PokemonBussines.swift
@@ -6,15 +6,20 @@
 //
 
 import Foundation
+
+// Definición de un alias para el tipo PokemonBusiness
 typealias PokemonBusiness = [PokemonName]
 
+// Definición de la estructura PokemonName, que representa una habilidad de un Pokemon
 struct PokemonName: Codable, Comparable {
-    let name: String
+    let name: String  // Nombre de la habilidad
     
+    // Inicializador que toma una Ability y un indicador booleano para indicar si la habilidad proviene de una solicitud combinada
     init(from ability: Ability, combine: Bool) {
         self.name = combine ? ability.ability.name.uppercased() : ability.ability.name
     }
 
+    // Método estático para comparar dos objetos PokemonName
     static func < (lhs: PokemonName, rhs: PokemonName) -> Bool {
         lhs.name < rhs.name
     }
